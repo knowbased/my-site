238ede56a5172670487b814c0f80bc4642fbe667
knowbased <60846047+knowbased@users.noreply.github.com>
2023-01-09 19:13:07 -0500

    feat(IconLink): :dizzy: add 3d rotation with cursor position
    add react-use + make custom hook useRotation

--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import useRotation from '../hooks/useRotation';
 
 import './IconLink.scss';
 
@@ -9,9 +10,18 @@ type IconLinkProp = {
 };
 
 const IconLink = ({ image, altImage, link }: IconLinkProp): JSX.Element => {
+  const ref = React.useRef(null);
+  const { rotationX, rotationY } = useRotation(ref);
+
   return (
-    <a href={link} className="iconLink">
-      <img src={image} alt={altImage} />
+    <a href={link} ref={ref} className="iconLink">
+      <img
+        style={{
+          transform: `perspective(4cm) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`,
+        }}
+        src={image}
+        alt={altImage}
+      />
     </a>
   );
 };
---
+++ src/hooks/useRotation.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { useHoverDirty, useMouseHovered } from 'react-use';
+
+const useRotation = (ref: React.MutableRefObject<null>) => {
+  const [rotationX, setRotationX] = useState(0);
+  const [rotationY, setRotationY] = useState(0);
+
+  const { elX, elY } = useMouseHovered(ref, {
+    bound: false,
+    whenHovered: true,
+  });
+
+  const isHovering = useHoverDirty(ref);
+
+  useEffect(() => {
+    if (isHovering) {
+      setRotationX(-((elY - 50) / 50) * 8);
+      setRotationY(((elX - 50) / 50) * 8);
+    } else {
+      setRotationX(0);
+      setRotationY(0);
+    }
+  }, [elX, elY, isHovering]);
+
+  return { rotationX, rotationY };
+};
+
+export default useRotation;
