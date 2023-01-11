9061c1ea046c8ae25e42a0bf90607ae97803e222
knowbased <60846047+knowbased@users.noreply.github.com>
2023-01-10 20:50:55 -0500

    feat(useRotation): :technologist: add return type

--- src/hooks/useRotation.tsx
+++ src/hooks/useRotation.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useHoverDirty, useMouseHovered } from 'react-use';
 
-const useRotation = (ref: React.MutableRefObject<null>) => {
+const useRotation = (
+  ref: React.MutableRefObject<null>
+): { rotationX: number; rotationY: number } => {
   const [rotationX, setRotationX] = useState(0);
   const [rotationY, setRotationY] = useState(0);
 
